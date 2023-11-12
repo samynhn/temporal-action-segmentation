ccac9da1e92b249108d3a1e4584a731acc116049
SHAWN! <t108820020@ntut.org.tw>
2023-11-12 22:34:21 +0800

    feature: extract_columns, drop_columns

--- filter.py
+++ filter.py
@@ -1,18 +1,62 @@
 import pandas as pd
-
-def extract_columns_from_csv(filename, columns_to_keep, output_filename):
-
-    df = pd.read_csv(filename)
-
-    new_df = df[columns_to_keep]
-
-    new_df.to_csv(output_filename, index=False)
-
-
+import numpy as np
 
 filename = 'data.csv'
-selected_columns = ['rally', 'ball_round', 'roundscore_A', 'lose_reason', 'win_reason']
-output_file = 'filteredData.csv'
-extract_columns_from_csv(filename, selected_columns, output_file)
+selected_columns = ['rally', 'ball_round', 'time', 'roundscore_A', 'roundscore_B', 'player', 'type', 'lose_reason', 'win_reason', 'getpoint_player', 'flaw', 'db']
+# NOTE: 有參考價值的欄位：rally, ball_round, time, roundscore_A, roundscore_B, player, type, lose_reason, win_reason, getpoint_player, flaw, db
+
+# columns_to_drop = ['']
+
+output_file = 'result/result.csv'
 
 
+def extract_columns(df, columns_to_keep):
+    new_df = df[columns_to_keep]
+    return new_df
+
+def save_to_csv(filename, data):
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
+def drop_columns(df, columns_to_drop):
+    new_df = df.drop(columns=columns_to_drop)
+    return new_df
+
+def segment(df):
+    # init
+    new_df = df.copy()
+    new_df.insert(0, 'rally_bool', np.nan)
+    new_df.insert(1, 'rally_player', np.nan)
+
+    flag = False # 回合結束時切換為1
+    rally_bool = False
+    rally_player = new_df.loc[0]["player"] # 以第一個球的發球者當作該回合擁有者
+
+    for index, row in new_df.iterrows():
+        if flag :
+            rally_bool = switch(rally_bool)
+            rally_player = row['player']        
+
+        new_df.at[index, 'rally_bool'] = rally_bool
+        new_df.at[index, 'rally_player'] = rally_player
+
+        if not pd.isna(row['getpoint_player']):  # 如果getpoint_player有值 代表此回合要結束了 新回合開始
+            flag = True # 切換回合 並在下次迴圈開始時寫入
+        else:
+            flag = False
+
+    return new_df
+
+
+def switch(bool):
+    return not bool
+
+org_df = pd.read_csv(filename)
+new_df_with_extract_columns = extract_columns(org_df, selected_columns)
+segment_df = segment(new_df_with_extract_columns)
+save_to_csv(output_file, segment_df)
+
+# NOTE:
+# 增加start, progress, end
+# True/False法 好像不太好
+# 遺失的frame 要補上
