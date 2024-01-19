e2c47aa6eb334d678616f1fb3590c5e3aaac0a4c
samynhn <a285.prgm@gmail.com>
2024-01-19 10:34:30 +0800

    fix: show cut frame info

--- cut_frame.py
+++ cut_frame.py
@@ -2,6 +2,7 @@ import csv
 file_csv = 'Kento_MOMOTA_CHOU_Tien_Chen_Fuzhou_Open_2019_Finals.mp4groundtruth.csv'
 remained_lines = 15000
 target = "break"
+
 def process_csv(file_path):
     with open(file_path, 'r', newline='', encoding='utf-8') as file:
         reader = list(csv.reader(file))
@@ -23,13 +24,13 @@ def process_csv(file_path):
             # 確定要保留的行數
             if (remained_lines-1 - upper_index) <= (lower_index - remained_lines-1):
                 nearest_index = upper_index
-                print("nearest index is upper_index: ", upper_index)
-                print(reader[upper_index][0]+1)
+                print("nearest index is upper_index: ", upper_index+1)
+                print(reader[upper_index][0])
             else:
                 nearest_index = lower_index
-                print("nearest index is lower_index: ", lower_index)
+                print("nearest index is lower_index: ", lower_index+1)
                 print(reader[lower_index][0]+1)
-            new_content = reader[:nearest_index + 1]
+            new_content = reader[:nearest_index]
 
         # 寫入新檔案
         with open('processed_' + file_path, 'w', newline='', encoding='utf-8') as new_file:
