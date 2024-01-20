5247c2f22f02a7279fed0e4e4313436a2b326898
samynhn <a285.prgm@gmail.com>
2024-01-20 22:59:50 +0800

    feature: load game from json file

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 from pathlib import Path
 
 FILE = Path(__file__).resolve()
@@ -12,21 +13,32 @@ import src.cut_frame as cut_frame # cut_frame.py
 import src.video as video # video.py
 
 if __name__ == "__main__":
-    game_name = 'Kento_MOMOTA_CHOU_Tien_Chen_Fuzhou_Open_2019_Finals.mp4'
-    rallySeg_name = "RallySeg.csv"
-    game_base_path = str(ROOT)+'/data/'+game_name+'/'
-    groundTruth_path = str(ROOT)+'/result/groundTruth/'+'groundTruth_'+game_name+'.csv'
-    videoPath = game_base_path + game_name
-    setFile_names = ['label/set1.csv', 'label/set2.csv', 'label/set3.csv']
-    setFiles_path = [os.path.join(game_base_path, setFile_name) for setFile_name in setFile_names]
-    actions = ["break", "break", "rally_start", "rally_end", "playerA", "playerB", "break"]
+    with open(str(ROOT)+'/data/data.json', 'r', encoding='utf-8') as json_file:
+        json_data = json.load(json_file)
 
-    remained_lines = 15000
-    target = "break"
+    # 使用for循環遍歷每個video對象
+    for game_info in json_data:
+        game_id = game_info["id"]
+        game_name = game_info["video"]
+        remained_lines = game_info["cut"]
 
-    output_video_path = str(ROOT)+'/result/video/'+str(remained_lines)+'_'+game_name
-    cut_groundTruth_path = str(ROOT)+'/result/groundTruth/'+str(remained_lines)+'_cut_groundTruth_'+game_name+'.csv'
+        rallySeg_name = "RallySeg.csv"
+        game_base_path = str(ROOT)+'/data/'+game_name+'/'
+        groundTruth_path = str(ROOT)+'/result/groundTruth/'+'groundTruth_'+game_name+'.csv'
+        videoPath = game_base_path + game_name
 
-    groundTruth.get_groundTruth(setFiles_path, game_base_path, groundTruth_path, videoPath, rallySeg_name, actions)
-    cut_frame.cut_groundTruth(groundTruth_path, remained_lines, cut_groundTruth_path, target)
-    video.save_video(videoPath, cut_groundTruth_path, output_video_path, cut=True, putText=True)
+        setFile_names =['label/set1.csv', 'label/set2.csv', 'label/set3.csv']
+        for setFile_name in setFile_names:
+            setFile_path = os.path.join(game_base_path, setFile_name)
+            if not os.path.exists(setFile_path):
+                setFile_names.remove(setFile_name)
+        setFiles_path = [os.path.join(game_base_path, setFile_name) for setFile_name in setFile_names]
+
+        actions = ["break", "break", "rally_start", "rally_end", "playerA", "playerB", "break"]
+        target = "break"
+        output_video_path = str(ROOT)+'/result/video/'+str(remained_lines)+'_'+game_name
+        cut_groundTruth_path = str(ROOT)+'/result/groundTruth/'+str(remained_lines)+'_cut_groundTruth_'+game_name+'.csv'
+
+        groundTruth.get_groundTruth(setFiles_path, game_base_path, groundTruth_path, videoPath, rallySeg_name, actions)
+        cut_frame.cut_groundTruth(groundTruth_path, remained_lines, cut_groundTruth_path, target)
+        video.save_video(videoPath, cut_groundTruth_path, output_video_path, cut=True, putText=True)
